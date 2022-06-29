cf1ad871dd74fd87355fb65b4732fb84607b521e
Thiago Porto <thiago.porto@d3.do>
2022-06-29 14:18:14 -0300

    feat: configure storybook and create a CI action
    Install and configure storybook and its addons, create and add logic to a CI github action
    
    #14

---
+++ .storybook/preview.tsx
@@ -0,0 +1,11 @@
+import "normalize.css/normalize.css"
+
+// TODO: When theme is setup finish this configuration
+
+// export const decorators = [
+// 	(Story) => (
+// 		<LibThemeProvider>
+// 			<Story />
+// 		</LibThemeProvider>
+// 	),
+// ]
