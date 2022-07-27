6325a4c750fee043c5246864c0fdc16a5d5f5fce
Thiago Porto <79651494+thiago-porto25@users.noreply.github.com>
2022-07-27 12:28:06 -0300

    Feature/preparation for release (#18)
    * chore: rename theme provider, update package.json and create README
    
    * chore: final changes for publishment
    
    Make all dependencies devDependencies, add files key in package.json, create a cd workflow file for
    release
    
    * fix: fix gh secret usage

---
+++ src/components/MinimThemeProvider/index.tsx
@@ -0,0 +1,1 @@
+export { MinimThemeProvider } from "./MinimThemeProvider"
--- src/test-utils/renderWithTheme.ts
+++ src/test-utils/renderWithTheme.ts
@@ -1,8 +1,8 @@
 import { render, RenderOptions } from "@testing-library/react"
-import { ChatRandomThemeProvider } from "../components"
+import { MinimThemeProvider } from "../components"
 
 const renderWithContexts = (ui: JSX.Element, options?: RenderOptions) => {
-	return render(ui, { wrapper: ChatRandomThemeProvider, ...options })
+	return render(ui, { wrapper: MinimThemeProvider, ...options })
 }
 
 export { renderWithContexts as render }
